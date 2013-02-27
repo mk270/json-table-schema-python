4491868951e3e16354723afc2e2aa5700392d1dc
Martin Keegan <martin@no.ucant.org>
2013-02-27 02:03:23 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+JSON Table Schema
+=================
+
+A Python library for handling JSON Table Schema files.
+
+See http://www.dataprotocols.org/en/latest/json-table-schema.html for details of the format
---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#coding: utf-8
+
+from setuptools import setup
+
+setup(
+	name = "json-table-schema",
+	author = "Martin Keegan",
+	author_email = "martin.keegan@okfn.org",
+	version = "0.1",
+	license = "Apache Licence v2.0",
+	url = "",
+	download_url = "",
+	description = "Python library for dealing with JSON Table Schema files",
+	py_modules = "",
+	packages = ["jsontableschema"],
+	install_requires = "",
+	scripts = ""
+)
